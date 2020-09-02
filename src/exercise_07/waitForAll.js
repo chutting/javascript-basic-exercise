60388c4f16ee9f0eaaffea4a5cae454b55e3cefd
chutting <tingting.chu@thoughtworks.com>
2020-09-03 01:14:12 +0800

    [Chu] feat: complete exercise_07

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -1,3 +1,5 @@
+const isPromise = obj => obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function';
+
 export default function waitForAll(...promises) {
   // This function returns a promise which will be triggered when all the `promises` are completed.
   //
@@ -9,5 +11,29 @@ export default function waitForAll(...promises) {
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  promises.forEach((ele) => {
+    if (!isPromise(ele)) {
+      throw new Error('Not all elements are promises.');
+    }
+  });
+
+  let isAllPromiseResolve = true;
+
+  const promise = new Promise((resolve, reject) => {
+    promises.forEach((ele, index) => {
+      ele.catch(() => {
+        isAllPromiseResolve = false;
+      }).then(() => {
+        if (index === promises.length - 1) {
+          if (isAllPromiseResolve) {
+            resolve();
+          } else {
+            reject();
+          }
+        }
+      });
+    });
+  });
+
+  return promise;
 }
