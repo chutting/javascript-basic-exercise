fd9fe9ff3e85391280d8363f45a2eb981cb49c59
chutting <tingting.chu@thoughtworks.com>
2020-09-02 18:39:46 +0800

    [Chu] feat: complete the exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -1,3 +1,10 @@
+/* eslint-disable no-param-reassign */
+const movePointer = (pointer, arr) => {
+  pointer += 1;
+  return (pointer === arr.length) ? null : { value: arr[pointer], next: movePointer(pointer, arr) };
+};
+/* eslint-enable no-param-reassign */
+
 export default function arrayToList(array) {
   // This function creates a linked list from an array.
   //
@@ -6,5 +13,17 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (!array) {
+    throw new Error('Creating list from undefined array');
+  } else if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+
+  const pointer = 0;
+  const list = {
+    value: array[pointer],
+    next: movePointer(pointer, array),
+  };
+
+  return list;
 }
