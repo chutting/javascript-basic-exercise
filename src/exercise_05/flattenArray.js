efd9e9a8f861ee26299e50c57bfb1757de64dbff
chutting <tingting.chu@thoughtworks.com>
2020-09-02 19:17:44 +0800

    [Chu] feat: complete the exercise_05

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -1,3 +1,12 @@
+// const flatMultiLayer = (ele, arr) => {
+//   if (Array.isArray(ele)) {
+//     ele.forEach(element => flatMultiLayer(element, arr));
+//   } else {
+//     arr.push(ele);
+//   }
+//   return arr;
+// };
+
 export default function flattenArray(array) {
   // This function flattens a nested array into a sequence.
   //
@@ -6,5 +15,26 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (!array) {
+    throw new Error('Flatten undefined or null array');
+  }
+
+  // * Use the flat function of Array in es6.
+  // return array.flat();
+
+  // * without es6.
+  const resultArr = [];
+  array.forEach((ele) => {
+    if (Array.isArray(ele)) {
+      ele.forEach((element) => {
+        resultArr.push(element);
+      });
+    } else {
+      resultArr.push(ele);
+    }
+  });
+  return resultArr;
+
+  // * multi layer flat without es6
+  // return flatMultiLayer(array, []);
 }
